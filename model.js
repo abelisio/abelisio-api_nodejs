078452c57215cdd4920459f1fa73f5d35f1ed290
AdrianoBelisio <66797966+abelisio@users.noreply.github.com>
2022-08-29 04:17:37 -1200

    Add files via upload

---
+++ model.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+ 
+var imageSchema = new mongoose.Schema({
+    name: String,
+    marc: String,
+    desc: String,
+    state: String,
+    img:
+    {
+        data: Buffer,
+        contentType: String
+    } 
+});
+ 
+module.exports = new mongoose.model('Image', imageSchema);
